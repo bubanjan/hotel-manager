142730214a4a90a02612cc1a6a6708e2ff027b24
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-04-04 00:02:15 +0200

    try catch in program.cs

--- HotelManager/HotelManagerAPI/Program.cs
+++ HotelManager/HotelManagerAPI/Program.cs
@@ -9,44 +9,54 @@ namespace HotelManagerAPI
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
             Log.Logger = new LoggerConfiguration()
                 .WriteTo.Console()
                  .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day,
                                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}")
                 .CreateLogger();
-
-            builder.Host.UseSerilog();
-
-            builder.Services.AddDbContext<HotelManagerDbContext>(options =>
-                    options.UseSqlite("Data Source=hotel.db"));
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddScoped<IGuestRepository, GuestRepository>();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Host.UseSerilog();
+
+                builder.Services.AddDbContext<HotelManagerDbContext>(options =>
+                        options.UseSqlite("Data Source=hotel.db"));
+
+                // Add services to the container.
+
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                builder.Services.AddScoped<IGuestRepository, GuestRepository>();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application startup failed!");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
     }
 }
