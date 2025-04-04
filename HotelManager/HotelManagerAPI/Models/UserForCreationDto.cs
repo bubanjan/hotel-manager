fecb204803d52fe2fbfa8034d976fae7eba663bd
Novak Bubanja <novak.bubanja@goteborgsregionen.se>
2025-04-05 01:16:49 +0200

    small change

--- HotelManager/HotelManagerAPI/Models/UserForCreationDto.cs
+++ HotelManager/HotelManagerAPI/Models/UserForCreationDto.cs
@@ -8,6 +8,7 @@ namespace HotelManagerAPI.Models
         public string UserName { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         [Required]
